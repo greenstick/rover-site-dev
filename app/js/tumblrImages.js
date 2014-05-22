deb5884b425ad4d027f1a9858fa16f7e5a2e2a39
Chad Giacopelli <cgiacopelli@columnfivemedia.com>
2014-05-22 10:52:35 -0700

    Text edit on index.html, added two new files for tumblr image retrieval.

---
+++ app/js/tumblrImages.js
@@ -0,0 +1,7 @@
+var tumblr = require('tumblr.js');
+var client = tumblr.createClient({
+  consumer_key: '<consumer key>',
+  consumer_secret: '<consumer secret>',
+  token: '<oauth token>',
+  token_secret: '<oauth token secret>'
+});
